7de86460cd0969d6f6b8f2ac5da9ee0527853c21
Abin Pillai <pillaiabincs232414@gmail.com>
2025-06-21 19:26:13 +0530

    Updated 'Joy Matters' section with logo and line spacing

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import VideoCarousel from './components/VideoCarousel';
 
+
 function App() {
   return (
     <div className="min-h-screen bg-white">
